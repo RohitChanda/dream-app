27e62f7352e5637fc0756e519c2e2cc7a12f3c6c
RohitChanda <>
2021-12-14 15:11:30 +0530

    change db to server

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/dreamapp",{
+// const DB="mongodb://localhost:27017/dreamapp";
+const DB="mongodb+srv://rohit:mydb@cluster0.yeqxw.mongodb.net/dreamApp?retryWrites=true&w=majority";
+mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>console.log("connection Sucessfull"))
--- backend/index.js
+++ backend/index.js
@@ -22,26 +22,10 @@ app.use(cokkieParser()); // to get the value from cookies in your browser
 // app.use(express.static(path.join(__dirname, '../public')));
 app.use('/public/uploadfile/userspost',express.static(path.join('public/uploadfile/userspost')));
 app.use('/public/uploadfile/userpic',express.static(path.join('public/uploadfile/userpic')));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname,'/../dist/dream-app/index.html'));
+});
 
-/*
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});*/
 //available routes
 app.use(router);
 
