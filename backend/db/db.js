fbe9b732e3b2ea478aeb6ff139e0d1f87b43eb22
RohitChanda <89380775+FalmingosDev@users.noreply.github.com>
2021-12-15 19:09:32 +0530

    eve

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 // const DB="mongodb://localhost:27017/dreamapp";
-const DB=process.env.DATABASE;
+const DB=process.env.DATABASE;//atlas
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true
