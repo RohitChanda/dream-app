9eeeb80227d3988076b135c71fbc100c8a659de0
RohitChanda <rohit.chanda.93@gmail.com>
2021-12-15 19:16:26 +0530

    eve example

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 // const DB="mongodb://localhost:27017/dreamapp";
-const DB=process.env.DATABASE;//mongodb atlas 
+const DB=process.env.DATABASE;//mongodb atlas   
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true
